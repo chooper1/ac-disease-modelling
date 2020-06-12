ffa8619009410fe1b736655b48d574ea8de0e66f
Margaret Hopkins <140948h@acadiau.ca>
2020-06-12 15:58:00 -0300

    debug SEIR modelling

--- R-model/Margaret/SEIR_fitting.R
+++ R-model/Margaret/SEIR_fitting.R
@@ -56,13 +56,13 @@ ssq_SEIR=function(par, region, active_cases, cases_F, cases_R, mu_CFR=0.01, phi,
   R_2=ode_soln[,"R_2"]
   F_=ode_soln[,"F_"]
   
-  ssq=sum((active_cases-(I_2+I_4))^2)+sum((cases_F-F_)^2)+sum((cases_R-R_2)^2)
+  ssq=sum(sqrt((active_cases-(I_2+I_4))^2))/mean(active_cases)+sum(sqrt((cases_F-F_)^2))/mean(cases_F)+sum(sqrt((cases_R-R_2)^2))/mean(cases_R)
   
   return(ssq)
   
 }
 
-fit_to_SEIR=function(region, C_data=JHU_C_data, F_data=JHU_F_data, R_data=JHU_R_data, mu_CFR=0.01, pop, beta_type="different"){
+fit_to_SEIR=function(region, C_data=JHU_C_data, F_data=JHU_F_data, R_data=JHU_R_data, mu_CFR=0.01, pop, beta_type="different", roll_size=1){
   region_name=regions(C_data)[region]
   
   phi=phi_vs_time(region, C_data, F_data, mu_CFR)
@@ -82,20 +82,20 @@ fit_to_SEIR=function(region, C_data=JHU_C_data, F_data=JHU_F_data, R_data=JHU_R_
   start=min(which(cases_C>0, arr.ind=TRUE))
   times=c(start:length(cases_C))
   cases_C=c(cases_C[start:length(cases_C)])
-  #cases_C=rollmean(cases_C, 5)
-  #times=rollmean(times, 5)
+  cases_C=rollmean(cases_C, roll_size)
+  times=rollmean(times, roll_size)
   
   cases_F=as.integer(F_data[5:nrow(F_data), region])
   cases_F=cases_F[!is.na(cases_F)]
   cases_F=c(cases_F[start:length(cases_F)])
- # cases_F=rollmean(cases_F, 5)
+  cases_F=rollmean(cases_F, roll_size)
   
   regions_R=regions(R_data)
   index_R=which(regions_R==region_name, arr.ind=TRUE)
   cases_R=as.integer(R_data[5:nrow(R_data), index_R])
   cases_R=cases_R[!is.na(cases_R)]
   cases_R=c(cases_R[start:length(cases_R)])
- # cases_R=rollmean(cases_R, 5)
+  cases_R=rollmean(cases_R, roll_size)
   
   active_cases=cases_C-cases_R
   
@@ -117,10 +117,11 @@ fit_to_SEIR=function(region, C_data=JHU_C_data, F_data=JHU_F_data, R_data=JHU_R_
   
 }
 
-
-plot_SEIR_fit=function(region, C_data=JHU_C_data, F_data=JHU_F_data, R_data=JHU_R_data, mu_CFR=0.01, pop, beta_type="different",fit_par=NULL, compartment){
+#compartment is "C", "F", or "R"
+#fit_param is output from fit_to_SEIR. If no parameters are passed, fit_to_SEIR will run
+plot_SEIR_fit=function(region, C_data=JHU_C_data, F_data=JHU_F_data, R_data=JHU_R_data, mu_CFR=0.01, pop, beta_type="different",fit_par=NULL, compartment, roll_size=1){
   if(length(fit_par==0)){
-    fit_par=fit_to_SEIR(region, C_data, F_data, R_data, mu_CFR, pop, beta_type)
+    fit_par=fit_to_SEIR(region, C_data, F_data, R_data, mu_CFR, pop, beta_type, roll_size=roll_size)
   }
   region_name=regions(C_data)[region]
   
@@ -141,14 +142,14 @@ plot_SEIR_fit=function(region, C_data=JHU_C_data, F_data=JHU_F_data, R_data=JHU_
   start=min(which(cases_C>0, arr.ind=TRUE))
   times=c(start:length(cases_C))
   cases_C=c(cases_C[start:length(cases_C)])
-  #cases_C=rollmean(cases_C, 5)
-  #times=rollmean(times, 5)
+  cases_C=rollmean(cases_C, roll_size)
+  times=rollmean(times, roll_size)
   C_df=data.frame(times, cases_C)
   
   cases_F=as.integer(F_data[5:nrow(F_data), region])
   cases_F=cases_F[!is.na(cases_F)]
   cases_F=c(cases_F[start:length(cases_F)])
-  # cases_F=rollmean(cases_F, 5)
+  cases_F=rollmean(cases_F, roll_size)
   F_df=data.frame(times, cases_F)
   
   regions_R=regions(R_data)
@@ -156,7 +157,7 @@ plot_SEIR_fit=function(region, C_data=JHU_C_data, F_data=JHU_F_data, R_data=JHU_
   cases_R=as.integer(R_data[5:nrow(R_data), index_R])
   cases_R=cases_R[!is.na(cases_R)]
   cases_R=c(cases_R[start:length(cases_R)])
-  # cases_R=rollmean(cases_R, 5)
+  cases_R=rollmean(cases_R, roll_size)
   R_df=data.frame(times, cases_R)
   
   active_cases=cases_C-cases_R
