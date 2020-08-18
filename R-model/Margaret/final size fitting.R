ec412d591cdebfac53ffbd5ed888fdbd1ab5df45
Margaret Hopkins <140948h@acadiau.ca>
2020-08-18 20:59:54 -0300

    final size fitting now working

--- R-model/Margaret/final size fitting.R
+++ R-model/Margaret/final size fitting.R
@@ -1,4 +1,5 @@
 library("deSolve")
+library("rootSolve")
 setwd("C:/Users/mjiho/ac-disease-modelling/R-model/Margaret/")
 source("fit_datasets_F.R")
 
@@ -18,12 +19,13 @@ rhs_SEIIAR=function(t, y, par, fit, N, phi){
     dE=beta*(I_1+I_2+A)*S/N-mu_E*E
     dI_1=mu_E*E-1.5*mu*I_1
     dI_2=1.5*(1/phi)*mu*I_1-mu*I_2
+    dC=1.5*(1/phi)*mu*I_1
     dA=1.5*(1-(1/phi))*mu*I_1-mu*A
     dR_1=mu*A
     dF_=(1-rho)*mu
     dR_2=rho*mu
     
-    list(c(dS, dE, dI_1, dI_2, dA, dR_1, dF_, dR_2))
+    list(c(dS, dE, dI_1, dI_2, dC, dA, dR_1, dF_, dR_2))
     
   })
 }
@@ -31,11 +33,12 @@ rhs_SEIIAR=function(t, y, par, fit, N, phi){
 ssq_SEIIAR=function(par, region, cases_C, cases_F, mu_IFR=0.01, phi, times, start, fit, pop, mu_CFR){
   
   p_pre_0=par[3]
-  #p_post_0=par[4]
+  p_post_0=par[4]
   
-  names=c("beta", "mu_E", "mu", "p_pre_0", "rho")
- # par=c(par[1], par[2], par[3], par[4], par[5], rho=1-mu_CFR)
-  par=c(par[1], par[2], 0.2, par[3], rho=1-mu_CFR)
+  names=c("beta", "mu_E", "mu", "p_pre_0", "p_post_0", "rho")
+  #par=c(par[1], par[2], par[3], par[4], par[5], rho=1-mu_CFR)
+ # par=c(par[1], par[2], 0.2, par[3], par[4], rho=1-mu_CFR)
+  par=c(par[1], 1/4, par[2], par[3], par[4], rho=1-mu_CFR)
   names(par)=names
   
   N=pop
@@ -47,31 +50,31 @@ ssq_SEIIAR=function(par, region, cases_C, cases_F, mu_IFR=0.01, phi, times, star
     phi_start=phi$ratios[index]
   }
   
-  names=c("S", "E", "I_1", "I_2", "A", "R_1", "F_", "R_2")
-  S0=c(S=N, E=N*p_pre_0/2, I_1=N*p_pre_0/2, I_2=cases_C[1], A=cases_C[1]*(1-(1/phi_start)), R_1=0, F_=cases_F[1], R_2=0)
+  names=c("S", "E", "I_1", "I_2", "C", "A", "R_1", "F_", "R_2")
+  S0=c(S=N, E=N*p_pre_0/2, I_1=N*p_pre_0/2, I_2=p_post_0*N/2, C=cases_C[1], A=p_post_0*N/2, R_1=0, F_=cases_F[1], R_2=0)
   names(S0)=names
   
-  p_post_0=(S0[5]+S0[4])/N
-  print(p_post_0)
-  
+  #p_post_0=(S0[5]+S0[4])/N
+
   ode_soln=ode(y=S0, times, func=rhs_SEIIAR, par=par, fit=fit, N=N, phi=phi)
   
-  I_2=ode_soln[,"I_2"]
-  R_2=ode_soln[,"R_2"]
+  #I_2=ode_soln[,"I_2"]
+  #R_2=ode_soln[,"R_2"]
+  C=ode_soln[,"C"]
   F_=ode_soln[,"F_"]
-  total_cases=I_2+R_2+F_
+  #total_cases=I_2+R_2+F_
   
-  ssq=sum(sqrt((cases_C-(total_cases))^2))/mean(cases_C)+sum(sqrt((cases_F-F_)^2))/mean(cases_F)
+  ssq=sum(sqrt((cases_C-C)^2))/mean(cases_C)+sum(sqrt((cases_F-F_)^2))/mean(cases_F)
   
   return(ssq)
   
 }
 
-fit_to_SEIIAR=function(region, C_data=JHU_C_data, F_data=JHU_F_data, mu_IFR=0.01, pop, intervention=1){
+fit_to_SEIIAR=function(region, C_data=JHU_C_data, F_data=JHU_F_data, mu_IFR=0.01, pop, intervention=1, final_size_guess=5000){
   
   region_name=regions(C_data)[region]
   
-  phi=phi_vs_time(region, C_data, F_data, mu_IFR)
+  phi=phi_vs_time(region, C_data, F_data, mu_IFR, final_size_guess = final_size_guess)
   
   tau_mu_CFR=fit_tau_mu_CFR(region, C_data, F_data)
   tau=tau_mu_CFR[1]
@@ -93,9 +96,9 @@ fit_to_SEIIAR=function(region, C_data=JHU_C_data, F_data=JHU_F_data, mu_IFR=0.01
   cases_F=cases_F[!is.na(cases_F)]
   cases_F=c(cases_F[start:length(cases_F)])
 
-  #par=c(beta=0.38, mu_E=1/4, mu=1/5, p_pre_0=0.001, p_post=0.001)
-  par=c(beta=0.38, mu_E=1/4, p_pre_0=0.001)
-  
+  #par=c(beta=0.38, mu_E=1/4, mu=1/5, p_pre_0=0.0001, p_post=0.0001)
+  #par=c(beta=0.38, mu_E=1/4, p_pre_0=0.0001, p_post_0=0.0001)
+  par=c(beta=0.38, mu=1/5, p_pre_0=0.0001, p_post_0=0.0001)
   
   ode_fit=optim(par=par, fn=ssq_SEIIAR, gr=NULL, region=region, cases_C=cases_C, cases_F=cases_F, mu_IFR=mu_IFR, phi=phi, times=times, start=start, fit=fit, pop=pop, mu_CFR=mu_CFR, method="L-BFGS-B", lower=c(0, 0, 0), upper=c(1, 1, 1))
   
@@ -111,3 +114,7 @@ plot_SEIIAR_fit=function(region, C_data=JHU_C_data, F_data=JHU_F_data, mu_IFR=0.
   
 }
 
+fn<-function(z){
+  0.7795282*z+log(1-z)-log(1-0.01068354)
+}
+uniroot(fn, c(0, 1))
